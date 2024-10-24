7a75b626d6fa6e833db1b8b5fdcbe554fc7a3654
Vivek Kumar <kumarvivek79510@gmail.com>
2024-10-24 20:33:01 +0530

    Restart future aspect of project

---
+++ Read.md
@@ -0,0 +1,5 @@
+Features to be added{
+  1) Multiple Posts by a single user.
+  2) User can access without signIn but can't post without signIn.
+  3) 
+}
--- app.js
+++ app.js
@@ -61,6 +61,7 @@ passport.use(new GoogleStrategy({
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
+    console.log(profile);
   }
 ));
 
